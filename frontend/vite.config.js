bb96dd52de5dbd905e718b25b62a1b8c66d7be07
Idonotwant <alps30318@gmail.com>
2024-04-24 16:43:20 +0800

    modify vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:8000",
+//         changeOrigin: true,
+//       },
+//     },
+//   },
+// });
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://lab05-ba4bw85u9-davids-projects-cd7aeacc.vercel.app/",
         changeOrigin: true,
       },
     },
